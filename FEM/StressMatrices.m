d9a469b4a7054c33e911a85aabf69ad3938493de
Tobbzn <Tobbzn@gmail.com>
2014-02-27 14:29:23 +0100

    Fikset stavefeil i variabelnavn.
    Endret
    B2s = Ep/(2+2*vs)*eye(3);
    til
    B2s = Es/(2+2*vs)*eye(3);
    
    Sånt skjer når man skriver "samme" funksjon to ganger. Denne burde kondenseres til bare StressMatrix og heller kalles to ganger.

--- FEM/StressMatrices.m
+++ FEM/StressMatrices.m
@@ -4,7 +4,7 @@ B2p = Ep/(2+2*vp)*eye(3);
 Cp = [B1p zeros(3); zeros(3) B2p];
 
 B1s = Es/(2*vs^2+vs-1)*[(vs-1) -vs -vs; -vs (vs-1) -vs; -vs -vs (vs-1)];
-B2s = Ep/(2+2*vs)*eye(3);
+B2s = Es/(2+2*vs)*eye(3);
 Cs = [B1s zeros(3); zeros(3) B2s];
 
 end
