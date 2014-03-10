3f308000f6d74561662a111c2e4eb7728d6e406e
Trygve Bærland <trygvba@null07.math.ntnu.no>
2014-03-10 17:34:43 +0100

    Endret Incorporate...

--- FEM/IncorporateDirichletBoundary.m
+++ FEM/IncorporateDirichletBoundary.m
@@ -1,10 +1,11 @@
-function [fnew Anew Mnew Unew] = IncorporateDirichletBoundary(A,M,U,upperNodes,lowerNodes,upperPlate,lowerPlate)
+function [fnew Anew Mnew Unew dUnew] = IncorporateDirichletBoundary(A,M,U,dU,upperNodes,lowerNodes,upperPlate,lowerPlate)
 lownode = sort(lowerNodes,'descend');
 uppnode = sort(upperNodes,'descend');
 allnode = sort([lowerNodes; upperNodes],'descend');
 Anew = A;
 Mnew = M;
 Unew = U;
+dUnew = dU;
 %Removing rows:
 for in=allnode
     i = 3*in; %z-coordinate.
@@ -12,10 +13,12 @@ for in=allnode
         Anew = Anew(1:(end-1),:);
         Mnew = Mnew(1:(end-1),:);
         Unew = Unew(1:(end-1));
+        dUnew = dUnew(1:(end-1));
     else
         Anew = Anew([1:(i-1) (i+1):end],:);
         Mnew = Mnew([1:(i-1) (i+1):end],:);
         Unew = Unew([1:(i-1) (i+1):end]);
+        dUnew = dUnew([1:(i-1) (i+1):end]);
     end
 end
 
