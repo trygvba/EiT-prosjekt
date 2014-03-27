91baa67d6fe91bd2ab671c461d14ee279584afb1
Trygve Bærland <trygvba@null21.math.ntnu.no>
2014-03-27 17:50:20 +0100

    Kalibrering av simTr...

--- FEM/simulationTrygveV6.m
+++ FEM/simulationTrygveV6.m
@@ -6,13 +6,14 @@ addpath(genpath('../Converters'));
 
 %Declaration of parameters:
 %Polymer:
+
 X = 15*10^(-6); %Length scale.
-Ep = 0.8*10^9/X;;
-vp = 0.46;
+Ep = 3*10^9*X;
+vp = 0.48;
 rhop = X^3*950;
 
 %Silver:
-Es = 72.4*10^9/X;
+Es = 72.4*10^9*X;
 vs = 0.37;
 rhos = X^3*10^4;
 
@@ -26,26 +27,35 @@ boundary = unique(tri);
 
 %--------------------------------------------
 disp('Assembly done')
+
+
+%Short eigenfrequency analysis:
+disp('Short eigenfrequency analysis')
+[V D] = eigs(A,M,20,'sm');
+omegas = sqrt(diag(D));
+
+
+%------------------------------
 disp('Setting up for time integration')
 
 %Parameters for time integration:
 T0=0;                       %start time.
 szU=size(A,1);              %dimension of our system.
-steps=2000;                   %Number of time steps.
+steps=100;                   %Number of time steps.
 %U = zeros(szU,steps);
 dt=10^(-9);                   %Temporal step size.
 OLT=0.01;                   %Outer Layer Thickness.
 impactzone=0.5;              %Parameter to decide which nodes are in the Dirichlet boundary.
 ballradius=max(p(:,3));     %Total radius of the ball with outher shell.
-omega=9*10^(6);                 %Frequency of upperplate.
+omega=omegas(7);                 %Frequency of upperplate.
 
 %Getting out nodes on the Dirichlet boundary:
 %Upper plate:
-    bound_up = ballradius-0.025;
+    bound_up = ballradius-0.02;
     [upperNodes, uz_up] = upperdirichletnodes(bound_up,p,zeros(szU,1),boundary);
     
 %Lower plate:
-    bound_low = -bound_up;
+    bound_low = -1.1*ballradius;
     [lowerNodes, uz_low] = lowerdirichletnodes(p,zeros(szU,1),bound_low,boundary);
 
 %Setting initial displacement:
@@ -71,6 +81,7 @@ I = eye(size(Amod,1));
 K2 = (I+0.25*dt^2*K1)\I;
 Mmod_inv = Mmod\I;
 
+%Setting up initial displacement:
 %Utemp_last = v;
 F_last = -Fmat_up*uz_up-Fmat_low*uz_low;
 Utemp_last = Amod\F_last;
@@ -86,11 +97,18 @@ output_folder = 'paraview/animation';
 title = 'testing';
 NumberOfPics = 100;
 n=1;
+
+%Finding top- and bottom node:
+index_bottom = find(p(:,3)==-ballradius);
+index_top = find(p(:,3)==ballradius);
+bottom_swing = zeros(steps,1);
+top_swing = zeros(steps,1);
+times = zeros(steps,1);
 tic
 for i=1:steps
     
     if i==1||(floor(NumberOfPics*i/steps)>floor(NumberOfPics*(i-1)/steps))
-        State_to_vtk(output_folder,title,n,szU,tetr(:,1:4),p,U);
+        State_to_vtk(output_folder,title,n,szU,tetr(:,1:4),p,U-u0);
         n = n+1;
     end
     t =T0+(i-1)*dt;
@@ -101,6 +119,15 @@ for i=1:steps
     v = v+ 0.5*dt*(Mmod_inv*(F_cur+F_last))-0.5*dt*K1*(Utemp_cur+Utemp_last);
     Utemp_last = Utemp_cur;
     F_last = F_cur;
+    bottom_swing(i)=U(3*index_bottom);
+    top_swing(i) = plateDisp(t);
+    times(i) = t;
+    
 end
 toc
+
+figure
+plot(times,bottom_swing,'r',times,top_swing,'b')
+title(sprintf('omega = %0.4e',omega))
+
 disp('Done')
